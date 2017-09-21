163ccb6c86f907e1f0179232509e893bfa511f96
James Moore <jameslmoore@gmail.com>
2017-09-20 23:51:38 -1000

    savepoint

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,24 +1,38 @@
 /* jshint esversion: 6 */
 
 angular.module('myApp')
-.controller('homeController', ['$scope', 'topicsProvider', function($scope, topicsProvider){
+.controller('homeController', ['$scope', 'topicsProvider', 'userProvider', function($scope, topicsProvider, userProvider){
   $scope.tempObj = "hello world stuff";
   $scope.testData = [];
   $scope.topicsData = [];
   $scope.currentTopic = '';
+  $scope.users = [];
+  $scope.usersVisable = false;
 
   $scope.topicChange = function(topic) {
-    console.log('topicChange', topic)
+    console.log('topicChange', topic);
     $scope.currentTopic = topic;
+  };
+
+  $scope.userToggle = function(){
+    console.log('toggle user function fired');
+    $scope.usersVisable = !$scope.usersVisable;
   };
 
   topicsProvider.getData()
   .then((data)=>{
-    console.log('topicsProvider data', data)
+    console.log('topicsProvider data', data);
     data.forEach(function(item){
-      $scope.topicsData.push(item.name)
-    })
-    //$scope.topicsData = data
+      $scope.topicsData.push(item.name);
+    });
   })
 
-}])
+  userProvider.getData()
+  .then((data)=>{
+    console.log('userProvider data in controller:', data);
+    data.forEach(function(item){
+      $scope.users.push(item.name);
+    });
+  });
+
+}]);
--- public/js/providers.js
+++ public/js/providers.js
@@ -1,4 +1,18 @@
 angular.module('myApp')
+.provider('userProvider', function() {
+  this.$get = ['$http', function($http) {
+    return {
+      getData: function(){
+        return $http.get('/api/users')
+        .then((data) =>{
+          console.log("my data is", data);
+          return data.data
+        })
+      }
+    }
+  }]
+})
+
 .provider('topicsProvider', function() {
   this.$get = ['$http', function($http) {
     return {
