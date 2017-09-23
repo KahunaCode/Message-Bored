381598099bd167aa28b5ba0d4df8f4447b76ac50
James Moore <jameslmoore@gmail.com>
2017-09-23 13:56:35 -1000

    fixed filter --> subfilter bug

--- public/js/controller.js
+++ public/js/controller.js
@@ -9,48 +9,31 @@ angular.module('myApp')
   $scope.users = [];
   $scope.usersVisable = false;
   $scope.messages = [];
+  $scope.messageTempHolder = [];
 
-  MessageService.getLatest()
-  .then((data) =>{
-    data.data.forEach(function(item){
-      console.log('items:', item);
-      let tempMessage = {
-        topic: item.topic_id,
-        msg: item.body
-      };
-      $scope.messages.push(tempMessage);
+  function getMessages(){
+    MessageService.getLatest()
+    .then((data) =>{
+      data.data.forEach(function(item){
+        console.log('items:', item);
+        let tempMessage = {
+          topic: item.topic_id,
+          msg: item.body
+        };
+        $scope.messages.push(tempMessage);
+      });
+      $scope.messageTempHolder = $scope.messages;
     });
-  });
+  }
+
+  getMessages();
 
   $scope.topicChange = function(topic) {
     $scope.currentTopic = topic;
     console.log('topicChange topic is', topic.id);
     console.log('messages', $scope.messages);
-    $scope.filteredMessages = $filter('filter')($scope.messages, {topic: topic.id});
-
-    console.log('filtered messages:', $scope.filteredMessages)
-    $scope.messages = $scope.filteredMessages;
-
-    // $scope.messages.filter(
-    //   function(topic){
-    //     console.log('$scope.messages', $scope.messages)
-    //     return ($scope.messages.id === topic.id);
-    //   }
-    //   );
+    $scope.messages = $filter('filter')($scope.messageTempHolder, {topic: topic.id});
   };
-
-
-  // $scope.topicChange = function(topic) {
-  //   console.log('topicChange', topic);
-  //   $scope.currentTopic = topic;
-  //   MessageService.getLatest()
-  //   .then((data)=>{
-  //     console.log(data.data);
-  //     data.data.forEach(function(item){
-  //       $scope.messages.push(item.body);
-  //     });
-  //   });
-  // };
 
   $scope.userToggle = function(){
     console.log('toggle user function fired');
