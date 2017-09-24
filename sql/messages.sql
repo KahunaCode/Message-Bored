5083b7b436c27b269929b4bab03503ff657949c9
James Moore <jameslmoore@gmail.com>
2017-09-23 15:17:14 -1000

    default database info

---
+++ sql/messages.sql
@@ -0,0 +1,13 @@
+
+
+
+insert into "Messages" values (default, 'And ittssss GOoooonnnneeeeeee!!!!!!', NOW(), NOW(), 2,4)
+insert into "Messages" values (default, 'sweet elixir of the gods', NOW(), NOW(), 1,2)
+insert into "Messages" values (default, 'Go Go Gadget GOLANG!', NOW(), NOW(), 1,4)
+insert into "Messages" values (default, 'awesome.js .. .what else is there to say', NOW(), NOW(), 1,3)
+insert into "Messages" values (default, 'JS rocks and it is great', NOW(), NOW(), 3,4)
+insert into "Messages" values (default, 'really, javascript is the best thing in the world oh yeah', NOW(), NOW(), 3,3)
+insert into "Messages" values (default, 'for sure, javascript is the best thing in the world', NOW(), NOW(), 3,3)
+insert into "Messages" values (default, 'elixir is the best thing in the world', NOW(), NOW(), 3,2)
+insert into "Messages" values (default, 'python is the best thing in the world', NOW(), NOW(), 3,1)
+insert into "Messages" values (default, 'python - its a wrap', NOW(), NOW(), 2,1)
---
+++ sql/topics.sql
@@ -0,0 +1,6 @@
+
+
+insert into "Topics" values (default, 'Python', NOW(), NOW(), 1)
+insert into "Topics" values (default, 'Elixir', NOW(), NOW(), 1)
+insert into "Topics" values (default, 'JavaScript', NOW(), NOW(), 1)
+insert into "Topics" values (default, 'GoLang', NOW(), NOW(), 1)
